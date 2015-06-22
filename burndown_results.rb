f2eb167054f8db008c59729f2188a9b8122791ea
Vlad Alive <vladalive@gmail.com>
2015-06-22 10:17:13 +0300

    [refactor] class to separate file

---
+++ burndown_results.rb
@@ -0,0 +1,90 @@
+require 'trello'
+
+class BurndownResults
+
+  attr_reader :board
+
+  RE_POINTS_FROM_NAME = /^\s*\(\s*([\d\.]*)\s*\)/
+
+  def initialize(options = {})
+    @board = Trello::Board.find(options[:board_id])
+    self
+  end
+
+  def print_personal_results
+    personal_results.each do |result|
+      puts "#{result[:member_name]}:"
+      puts "  Cards : #{result[:cards_percentage]}% (#{result[:cards_done]} / #{result[:cards_total]}), #{result[:cards_undone]} left"
+      puts "  Points: #{result[:points_percentage]}% (#{result[:points_done]} / #{result[:points_total]}), #{result[:points_undone]} left"
+    end
+  end
+
+  private
+
+  def personal_results
+    @board.members
+      .map { |member|
+        cards_with_estimates
+          .select{|card| card[:member_id] == member.id}
+          .reduce({
+            member_id: member.id,
+            member_name: member.full_name,
+            cards_total: 0,
+            cards_done: 0,
+            cards_undone: 0,
+            points_total: 0,
+            points_done: 0,
+            points_undone: 0,
+          }) {|memo, card|
+            if card[:list_id] == done_list.id
+              memo[:cards_done] += 1
+              memo[:points_done] += card[:estimate]
+            else
+              memo[:cards_undone] += 1
+              memo[:points_undone] += card[:estimate]
+            end
+            memo[:cards_total] += 1
+            memo[:points_total] += card[:estimate]
+            memo
+          }
+          .tap{|result|
+            result[:cards_percentage] = (1.0 * result[:cards_done] / (result[:cards_total] + 0.0001) * 100).round
+            result[:points_percentage] = (1.0 * result[:points_done] / (result[:points_total] + 0.0001) * 100).round
+          }
+      }
+      .reject{|result| result[:cards_total] == 0 }
+      .sort_by{|result| 1 * result[:points_undone]}
+  end
+
+  def cards_with_estimates
+    @cards_with_estimates ||=
+      @board.cards
+        .map{ |card|
+          match = card.name.match(RE_POINTS_FROM_NAME)
+          estimate = match.present? && match[1].present? && match[1].to_f || 0
+          {
+            card: card,
+            card_name: card.name,
+            estimate: estimate,
+            member_id: card.member_ids.first,
+            list_id: card.list_id,
+          }
+        }
+        .reject{ |obj|
+          obj[:estimate] == 0
+        }
+  end
+
+  def done_list
+    @done_list ||=
+      @board.lists
+        .find{|list| list.attributes[:name] == 'Done'}
+  end
+
+  def undone_lists
+    @undone_lists ||=
+      @board.lists
+        .reject{|list| list.id == done_list.id}
+  end
+
+end
--- script.rb
+++ script.rb
@@ -1,108 +1,20 @@
 #!/usr/bin/env ruby
 
-require 'trello'
-require 'yaml'
 require 'dotenv'
+require 'trello'
+require './burndown_results'
 
 Dotenv.load
-
-board_id = ENV['TRELLO_BOARD_ID']
 
 Trello.configure do |config|
   config.developer_public_key = ENV['TRELLO_DEV_PUB']
   config.member_token = ENV['TRELLO_MEMBER_TOKEN']
 end
 
-class BurndownResults
+board_id = ENV['TRELLO_BOARD_ID']
 
-  attr_reader :board
-
-  RE_POINTS_FROM_NAME = /^\s*\(\s*([\d\.]*)\s*\)/
-
-  def initialize(options = {})
-    @board = Trello::Board.find(options[:board_id])
-    self
-  end
-
-  def print_personal_results
-    personal_results.each do |result|
-      puts "#{result[:member_name]}:"
-      puts "  Cards : #{result[:cards_percentage]}% (#{result[:cards_done]} / #{result[:cards_total]}), #{result[:cards_undone]} left"
-      puts "  Points: #{result[:points_percentage]}% (#{result[:points_done]} / #{result[:points_total]}), #{result[:points_undone]} left"
-    end
-  end
-
-  private
-
-  def personal_results
-    @board.members
-      .map { |member|
-        cards_with_estimates
-          .select{|card| card[:member_id] == member.id}
-          .reduce({
-            member_id: member.id,
-            member_name: member.full_name,
-            cards_total: 0,
-            cards_done: 0,
-            cards_undone: 0,
-            points_total: 0,
-            points_done: 0,
-            points_undone: 0,
-          }) {|memo, card|
-            if card[:list_id] == done_list.id
-              memo[:cards_done] += 1
-              memo[:points_done] += card[:estimate]
-            else
-              memo[:cards_undone] += 1
-              memo[:points_undone] += card[:estimate]
-            end
-            memo[:cards_total] += 1
-            memo[:points_total] += card[:estimate]
-            memo
-          }
-          .tap{|result|
-            result[:cards_percentage] = (1.0 * result[:cards_done] / (result[:cards_total] + 0.0001) * 100).round
-            result[:points_percentage] = (1.0 * result[:points_done] / (result[:points_total] + 0.0001) * 100).round
-          }
-      }
-      .reject{|result| result[:cards_total] == 0 }
-      .sort_by{|result| 1 * result[:points_undone]}
-  end
-
-  def cards_with_estimates
-    @cards_with_estimates ||=
-      @board.cards
-        .map{ |card|
-          match = card.name.match(RE_POINTS_FROM_NAME)
-          estimate = match.present? && match[1].present? && match[1].to_f || 0
-          {
-            card: card,
-            card_name: card.name,
-            estimate: estimate,
-            member_id: card.member_ids.first,
-            list_id: card.list_id,
-          }
-        }
-        .reject{ |obj|
-          obj[:estimate] == 0
-        }
-  end
-
-  def done_list
-    @done_list ||=
-      @board.lists
-        .find{|list| list.attributes[:name] == 'Done'}
-  end
-
-  def undone_lists
-    @undone_lists ||=
-      @board.lists
-        .reject{|list| list.id == done_list.id}
-  end
-
-end
-
-@results = BurndownResults.new({
+results = BurndownResults.new({
   board_id: board_id,
 })
-@results.print_personal_results
+
+results.print_personal_results
